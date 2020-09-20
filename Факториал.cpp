a634eb83ed7189cf321a760b9d02329f15e24906
Yegor <54379419+MorgooN@users.noreply.github.com>
2020-09-20 13:30:52 +0300

    Add files via upload

---
+++ Факториал.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+
+
+void printBinary(int x)
+{
+	if (x == 0)
+		return;
+
+	printBinary(x / 2);
+
+	std::cout << % 2;
+}
+
+int main()
+{
+	int x;
+	std::cout << "enter a integer";
+	std::cin >> x;
+
+	printBinary(x);
+	return 0;
+}
