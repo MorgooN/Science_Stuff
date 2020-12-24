07aa510b699ea63700e8da5eb42900ab9ca42f90
Yegor <54379419+MorgooN@users.noreply.github.com>
2020-12-25 00:47:07 +0300

    Add files via upload

---
+++ ПереворотСтроки.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp62
+{
+    public partial class Form1 : Form
+    {
+        Graphics Графика; Single Угол;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Смена изображения";
+            button1.Text = "Развернуть";
+            Угол = 0.0F;
+            panel1.Location = new Point(12, 12);
+            panel1.Size = new System.Drawing.Size(240, 200);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            panel1.BackColor = Color.AliceBlue;
+            panel1.Cursor = Cursors.Cross;
+            Графика = e.Graphics;
+            Графика.TranslateTransform(120.0F, 100.0F);
+            Графика.RotateTransform(Угол);
+            Графика.DrawString("PRIMO VICTORIA!", new Font("Comic Sans MS", 12, FontStyle.Bold ^ FontStyle.Italic),
+            Brushes.Red, -70, -15);
+            Графика.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Угол = Угол + 30.0F;
+            panel1.Invalidate();
+        }
+    }
+}
---
+++ РисованиеМышью.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp61
+{
+    public partial class Form1 : Form
+    {
+        Boolean Рисовать_ли;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Рисую мышью в форме";
+            button1.Text = "Стереть";
+            Рисовать_ли = false;
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Рисовать_ли = false;
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Рисовать_ли = true;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Рисовать_ли == true)
+            {
+                var Графика = CreateGraphics();
+                Графика.FillRectangle(new SolidBrush(Color.Red), e.X, e.Y, 10, 10);
+                Графика.Dispose();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Графика = CreateGraphics();
+            Графика.Clear(this.BackColor);
+        }
+    }
+}
+
